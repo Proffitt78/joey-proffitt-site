653584e52c200b3531aab2f8b5a215cf26cc6862
Proffitt78 <joeyproffitt78@gmail.com>
2025-09-18 11:41:35 -0400

    attempting to fix CORS issue

--- joey-proffitt-site/src/plugins/axios.ts
+++ joey-proffitt-site/src/plugins/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
 //   baseURL: "https://localhost:7214/api", // adjust to your API base URL
-  baseURL: "https://joey-proffitt-portfolio-gvbzc7btfkcpeccm.westus2-01.azurewebsites.net/api", // adjust to your API base URL
+  baseURL: "/api", // adjust to your API base URL
 });
 
 export default api;
